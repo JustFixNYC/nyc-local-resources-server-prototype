6ceee8936c64489cd4807ff6d9b5c495b6a5ab89
Atul Varma <varmaa@gmail.com>
2018-10-21 16:50:32 -0400

    Show a union of two zipcodes.

--- nyc/views.py
+++ nyc/views.py
@@ -1,16 +1,23 @@
 import json
 from django.shortcuts import render
 from django.conf import settings
+from django.contrib.gis.geos import GEOSGeometry
 
 from .models import Zipcode
 
 
 def index(request):
-    origin = Zipcode.objects.get(postalcode="11201")
+    total_area = GEOSGeometry('POINT EMPTY', srid=4326)
+    zipcodes = ['11201', '11231']
+    areas = Zipcode.objects.filter(postalcode__in=zipcodes).all()
+    for area in areas:
+        total_area = total_area.union(area.geom)
+    origin = list(reversed(total_area.centroid))
+
     js_params = {
         'mapboxAccessToken': settings.MAPBOX_ACCESS_TOKEN,
-        'origin': [origin.latitude, origin.longitude],
-        'area': json.loads(origin.geom.geojson)
+        'origin': origin,
+        'area': json.loads(total_area.geojson)
     }
     return render(request, 'index.html', {
         'js_params': js_params
